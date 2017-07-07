e0aa04a84e0caab24f0ed822da8ebc93f83bce25
Bingtao LI <li1bing6tao8>
2017-07-07 14:01:46 +0800

    成功

--- prictice/继续害死人.cpp
+++ prictice/继续害死人.cpp
@@ -1,4 +1,21 @@
 #include<iostream>
+int count = 0;
+void sort(int *arr, int K)
+{
+	int temp;
+	for (int i = 0; i < K; i++) 
+	{
+		for (int j = i+1; j < K; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
 int main()
 {
 	int K;
@@ -8,12 +25,38 @@ int main()
 	{
 		std::cin >> arr[i];
 	}
-	
+
 	for (int i = 0; i < K; i++)
 	{
-		if (i == (K - 1)) { std::cout << arr[i]; }
+		int t = arr[i];
+		if (t == 0)
+		{
+			continue;
+		}
+		while (t != 1)
+		{
+			
+			if (t % 2 == 0) { t /= 2; }
+			else { t = (3 * t + 1) / 2; }
+			for (int j = 0; j < K; j++)
+			{
+				if (arr[j] == t)
+				{ 
+					arr[j] = 0; 
+					break;
+				}
+			}
+		}
+	}
+	sort(arr, K);
+	int count = 0;
+	while (arr[count] != 0) { count++;	}
+	for (int i = 0; i < count; i++)
+	{
+		if (i == (count - 1)) { std::cout << arr[i]; }
 		else { std::cout << arr[i] << " "; }
 	}
 	std::cin.get();
 	std::cin.get();
+	return 0;
 }
