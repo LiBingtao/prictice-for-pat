d3ad326dd0d5a005bcb0f367f3da732fadea1eb4
Bingtao LI <li1bing6tao8>
2017-08-02 14:51:28 +0800

    success

---
+++ Advanced Level/1001. A+B Format.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>  
+#include <iostream>  
+#include <string>  
+#include <algorithm>  
+
+using namespace std;
+
+int main()
+{
+	int a, b, sum, n, i;
+
+	n = 1, i = 1;
+	cin >> a >> b;
+	sum = a + b;
+	bool isneg = false;
+	if (sum<0)
+	{
+		isneg = true;
+		sum = 0 - sum;
+	}
+	string str;
+	if (isneg)
+	{
+		cout << '-';
+	}
+	if (sum < 1000) {
+		cout << sum << endl;
+	}
+	else {
+		while (sum)
+		{
+			char ch = '0' + sum % 10;
+			sum /= 10;
+			if (i % 4 == 0)
+			{
+				str.push_back(',');
+				i = 1;
+			}
+			str.push_back(ch);
+			i++;
+		}
+		reverse(str.begin(), str.end());
+		cout << str << endl;
+
+	}
+	system("PAUSE");
+	return 0;
+}
