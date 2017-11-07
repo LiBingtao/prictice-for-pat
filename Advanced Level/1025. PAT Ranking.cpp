54bdf8bb293dc5cb53e61e84b1995ad14223e538
李冰涛 <67784273@qq.com>
2017-11-07 22:28:02 +0800

    aaa

--- Advanced Level/1025. PAT Ranking.cpp
+++ Advanced Level/1025. PAT Ranking.cpp
@@ -13,34 +13,29 @@ bool cmp(stu a,stu b){
 	return (a.grade==b.grade)? a.id<b.id : a.grade>b.grade;
 }
 int main(){
-	vector<vector<stu>> v;
 	vector<stu> vt;
 	int n;
 	cin>>n;
-	v.resize(n);
 	for (int i=0;i<n;i++){
 		int k;
 		cin>>k;
+		vector<stu> v(k);
 		for (int j=0;j<k;j++){
-			stu temp;
-			scanf("%lld %d",&temp.id,&temp.grade);
-			temp.local_id = i+1;
-			v[i].push_back(temp);
+			scanf("%lld %d",&v[j].id,&v[j].grade);
+			v[j].local_id = i+1;
 		}
-	}
-	for (int i=0;i<n;i++){
-		sort(v[i].begin(),v[i].end(),cmp);
-		v[i][0].loca_rank = 1;
-        vt.push_back(v[i][0]);
+		sort(v.begin(),v.end(),cmp);
+		v[0].loca_rank = 1;
+        vt.push_back(v[0]);
 		for(u_long j=0;j<v[i].size();j++){
 			v[j].loca_rank = (v[j].grade == v[j - 1].grade) ? (v[j - 1].loca_rank) : (j + 1);
-			vt.push_back(v[i][j]);
+			vt.push_back(v[j]);
 		}
 	}
 	sort(vt.begin(),vt.end(),cmp);
 	vt[0].rank = 1;
 	for(u_long i=0;i<vt.size();i++){
-		vt[i].rank = ((vt[i].grade == vt[i-1].grade)?(v[i-1].rank):(i+1));	
+		vt[i].rank = ((vt[i].grade == vt[i-1].grade)?(vt[i-1].rank):(i+1));	
 	}
 	cout<<vt.size()<<endl;
 	for(u_long i=0;i<vt.size();i++){
