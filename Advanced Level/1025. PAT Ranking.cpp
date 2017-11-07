dd45490658b2da1054fecb7d24bc6f669c817487
李冰涛 <67784273@qq.com>
2017-11-07 22:40:46 +0800

    AC

--- Advanced Level/1025. PAT Ranking.cpp
+++ Advanced Level/1025. PAT Ranking.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
 #include<string>
+#include<vector>
 using namespace std;
 struct stu{
 	long long id;
@@ -27,19 +28,19 @@ int main(){
 		sort(v.begin(),v.end(),cmp);
 		v[0].local_rank = 1;
         	vt.push_back(v[0]);
-		for(u_long j=0;j<v.size();j++){
+		for(int j=1;j<v.size();j++){
 			v[j].local_rank = (v[j].grade == v[j - 1].grade) ? (v[j - 1].local_rank) : (j + 1);
 			vt.push_back(v[j]);
 		}
 	}
 	sort(vt.begin(),vt.end(),cmp);
 	vt[0].rank = 1;
-	for(u_long i=0;i<vt.size();i++){
+	for(int i=1;i<vt.size();i++){
 		vt[i].rank = ((vt[i].grade == vt[i-1].grade)?(vt[i-1].rank):(i+1));	
 	}
 	cout<<vt.size()<<endl;
 	for(u_long i=0;i<vt.size();i++){
-		printf("%13d %d %d %d\n",vt[i].id,vt[i].rank,vt[i].local_id,vt[i].local_rank);
+		printf("%013lld %d %d %d\n",vt[i].id,vt[i].rank,vt[i].local_id,vt[i].local_rank);
 	}
 	return 0;
 }
