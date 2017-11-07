38ba8cc935527ff604fd02a4e1d432505ce8cb04
李冰涛 <67784273@qq.com>
2017-11-07 22:33:01 +0800

    Update 1025. PAT Ranking.cpp

--- Advanced Level/1025. PAT Ranking.cpp
+++ Advanced Level/1025. PAT Ranking.cpp
@@ -25,10 +25,10 @@ int main(){
 			v[j].local_id = i+1;
 		}
 		sort(v.begin(),v.end(),cmp);
-		v[0].loca_rank = 1;
-        vt.push_back(v[0]);
-		for(u_long j=0;j<v[i].size();j++){
-			v[j].loca_rank = (v[j].grade == v[j - 1].grade) ? (v[j - 1].loca_rank) : (j + 1);
+		v[0].local_rank = 1;
+        	vt.push_back(v[0]);
+		for(u_long j=0;j<v.size();j++){
+			v[j].local_rank = (v[j].grade == v[j - 1].grade) ? (v[j - 1].local_rank) : (j + 1);
 			vt.push_back(v[j]);
 		}
 	}
@@ -39,7 +39,7 @@ int main(){
 	}
 	cout<<vt.size()<<endl;
 	for(u_long i=0;i<vt.size();i++){
-		printf("%13d %d %d %d\n",vt.id,vt.rank,vt.local_id,vt.local_rank);
+		printf("%13d %d %d %d\n",vt[i].id,vt[i].rank,vt[i].local_id,vt[i].local_rank);
 	}
 	return 0;
 }
