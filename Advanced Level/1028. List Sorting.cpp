1beacf87af2293cac732d699160fb6c01682cb5f
李冰涛 <67784273@qq.com>
2017-11-08 22:22:51 +0800

    AC

--- Advanced Level/1028. List Sorting.cpp
+++ Advanced Level/1028. List Sorting.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 struct stu{
 	int id,score;
-	string name;
+	char name[10];
 };
 bool cmp1(stu a,stu b){
 	return a.id<b.id;
 }
 bool cmp2(stu a,stu b){
-	return (a.name==b.name)?a.id<b.id:a.name<b.name;	
+	return (strcmp(a.name,b.name)==0)?a.id<b.id:strcmp(a.name, b.name) < 0;	
 }
 bool cmp3(stu a,stu b){
 	return (a.score==b.score)?a.id<b.id:a.score<b.score;	
@@ -21,19 +21,13 @@ int main(){
 	cin>>n>>c;
 	vector<stu> student(n);
 	for(u_long i=0;i<n;i++){
-		int a,d;
-		string b;
-		cin>>a>>b>>d;
-		student[i].id = a;
-		student[i].name = b;
-		student[i].score = d;
+		scanf("%d %s %d", &student[i].id, student[i].name, &student[i].score);
 	}
 	if(c==1) sort(student.begin(),student.end(),cmp1);
 	if(c==2) sort(student.begin(),student.end(),cmp2);
 	if(c==3) sort(student.begin(),student.end(),cmp3);
 	for(u_long i = 0; i < n; i++) {
-        printf("%06d ", student[i].id);
-		cout<<student[i].name<<" "<<student[i].score<<endl;
+		printf("%06d %s %d\n", student[i].id, student[i].name, student[i].score);
     }
     return 0;
 }
