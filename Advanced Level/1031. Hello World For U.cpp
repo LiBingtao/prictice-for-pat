5e2d91cffcf48267a15decb02ca74e67033f9f12
李冰涛 <67784273@qq.com>
2017-11-09 15:52:04 +0800

    AC

--- Advanced Level/1031. Hello World For U.cpp
+++ Advanced Level/1031. Hello World For U.cpp
@@ -5,12 +5,9 @@ int main(){
 	string str;
 	cin>>str;
 	int n = str.length();
-	int n1,n2=3;
-	n1 = (n+2-n2)/2;
-	while(n1>n2){
-		n2++;
-		n1 = (n+2-n2)/2;
-	}
+	int n1,n2;
+	int n1 = n / 3;
+    	int n2 = n1 + n % 3;
 	int i=0;
 	for(;i<n1-1;i++){
 		cout<<str[i];
