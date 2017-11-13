19616368b018b590b9c991ef57c443b5723bc2a9
李冰涛 <67784273@qq.com>
2017-11-13 20:51:09 +0800

    简单版

--- Advanced Level/1038. Recover the Smallest Number.cpp
+++ Advanced Level/1038. Recover the Smallest Number.cpp
@@ -3,30 +3,7 @@
 #include<algorithm>
 using namespace std;
 bool cmp(string a,string b){
-    if (a.length() == b.length()){
-        return a<b;
-    }else{
-       if(a.length()>b.length()){
-           int size = b.length();
-           string sub1 = a.substr(0,size);
-           string sub2 = a.substr(size,a.length()-size);
-           if(sub1 != b){
-               return sub1<b;
-           }else{
-               return sub2<b;
-           }
-       } 
-       else{
-           int size = a.length();
-           string sub1 = b.substr(0,size);
-           string sub2 = b.substr(size,b.length()-size);
-           if(sub1 != a){
-               return a<sub1;
-           }else{
-               return a<sub2;
-           }
-       }
-    }
+    return a+b<b+a; 
 }
 int main(){
     int n;
