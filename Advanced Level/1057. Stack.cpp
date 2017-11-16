b36dc66664397169658515cb62d6892de9394a99
李冰涛 <67784273@qq.com>
2017-11-16 12:26:18 +0800

    树状数组AC

--- Advanced Level/1057. Stack.cpp
+++ Advanced Level/1057. Stack.cpp
@@ -1,33 +1,58 @@
 #include<iostream>
-#include<vector>
+#include<stack>
 #include<algorithm>
-#include<string>
 using namespace std;
+#define lowbit(i) ((i)&(-i))
+const int maxn=100010;
+int c[maxn];
+stack<int> q;
+void update(int x,int v){
+    for(int i=x;i<maxn;i+=lowbit(i)){
+        c[i]+=v;
+    }
+}
+int getsum(int x){
+    int sum = 0;
+    for(int i=x;i>0;i-=lowbit(i)){
+        sum+=c[i];
+    }
+    return sum;
+}
+void PeekMedian() {
+    int left = 1, right = maxn, mid, k = (q.size() + 1) / 2;
+    while(left < right) {
+        mid = (left + right) / 2;
+        if(getsum(mid) >= k)
+            right = mid;
+        else
+            left = mid + 1;
+    }
+    printf("%d\n", left);
+}
 int main(){
-    vector<int>q,t;
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        string s;
-        cin>>s;
-        if(s=="Pop"){
+        char s[15];
+        scanf("%s",s);
+        if(s[1]=='o'){
             if(q.size()==0){
                 cout<<"Invalid"<<endl;
             }else{
-                cout<<q.back()<<endl;
-                q.pop_back();
+                cout<<q.top()<<endl;
+                update(q.top(),-1);
+                q.pop();
             }
-        }else if(s=="Push"){
+        }else if(s[1]=='u'){
             int k;
             cin>>k;
-            q.push_back(k);
+            q.push(k);
+            update(k,1);
         }else{
             if(q.size()==0){
                 cout<<"Invalid"<<endl;
             }else{
-                t = q;
-                sort(t.begin(),t.end());
-                cout<<t[(t.size()-1)/2]<<endl;
+                PeekMedian();
             } 
         }
     }
