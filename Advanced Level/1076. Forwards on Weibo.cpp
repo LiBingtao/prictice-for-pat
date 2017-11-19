59ce4ae2c87c886fcc5f4d44d00b0eb9a5103cb0
李冰涛 <67784273@qq.com>
2017-11-19 22:19:18 +0800

    AC

--- Advanced Level/1076. Forwards on Weibo.cpp
+++ Advanced Level/1076. Forwards on Weibo.cpp
@@ -1,19 +1,45 @@
 #include<cstdio>
 #include<vector>
+#include<queue>
 using namespace std;
-int res;
 int n,l;
 bool hasforward[1010];
 vector<int> v[1010];
-void dfs(int p,int dep){
-    if(hasforward[p]==false){
-        res++;
-        hasforward[p]=true;
+// void dfs(int p,int dep){
+//     if(hasforward[p]==false){
+//         res++;
+//         hasforward[p]=true;
+//     }
+//     if(dep==l || v[p].size()==0) return;
+//     for(int i=0;i<v[p].size();i++){
+//         dfs(v[p][i],dep+1);
+//     }
+// }
+int bfs(int p){
+    queue<int> q;
+    q.push(p);
+    hasforward[p]=true;
+    int res = 0,dep=0,first=0,last=1;
+    while(!q.empty()){
+        int top = q.front();
+        q.pop();
+        first++;
+        for(int i=0;i<v[top].size();i++){
+            if(!hasforward[v[top][i]] && dep<=l){
+                q.push(v[top][i]);
+                hasforward[v[top][i]] = true;
+                res++;
+            }
+        }
+        if(first==last){
+            last = res+1;
+            dep++;
+        }
+        if(dep==l){
+            return res;
+        }
     }
-    if(dep==l || v[p].size()==0) return;
-    for(int i=0;i<v[p].size();i++){
-        dfs(v[p][i],dep+1);
-    }
+    return res;
 }
 int main(){
     scanf("%d %d",&n,&l);
@@ -32,11 +58,9 @@ int main(){
     for(int i=0;i<k;i++){
         int t;
         scanf("%d",&t);
-        res = 0;
         fill(hasforward+1,hasforward+n+1,false);
         hasforward[t]=true;
-        dfs(t,0);
-        printf("%d\n",res);
+        printf("%d\n",bfs(t));
     }
     return 0;
 }
