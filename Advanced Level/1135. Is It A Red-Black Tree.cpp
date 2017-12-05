20771fed0ea93fe5a233526d66fc1128be33500e
李冰涛 <67784273@qq.com>
2017-12-05 17:22:16 +0800

    AC

--- Advanced Level/1135. Is It A Red-Black Tree.cpp
+++ Advanced Level/1135. Is It A Red-Black Tree.cpp
@@ -1,46 +1,69 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-const int Size = 100000;
-int tree[Size],pre[Size];
+struct node{
+    int v,left,right;
+};
+const int Size = 32;
+int pre[Size];
+node tree[Size];
 int n,m,finalblack;
 bool is=true;
-void build(int left,int right,int index){
+void build(int left,int right){
+    if(left==right) {
+        tree[left].v = pre[left];
+        tree[left].left = -1;
+        tree[left].right = -1;
+        return;
+    }
     if(left>right) return;
-    tree[index] = pre[left];
+    tree[left].v = pre[left];
     int i=left+1;
-    while(abs(pre[i])<abs(pre[left]) && i<m) i++;
-    build(left+1,i-1,index*2+1);
-    build(i,right,index*2+2);
+    while(abs(pre[i])<abs(pre[left]) && i<=right) i++;
+    if(left+1<=right && abs(pre[left+1]) < abs(pre[left])) tree[left].left = left+1;
+    else tree[left].left = -1;
+    if(i<=right) tree[left].right = i;
+    else tree[left].right = -1;
+    build(left+1,i-1);
+    build(i,right);
 }
 void dfs(int root,int black){
-    if(tree[root]==0){
+    if(tree[root].v>0) black+=1;
+    if(tree[root].left==-1 && tree[root].right==-1){
         if(black!=finalblack) is = false;
         return;
     }
-    if(tree[root]>0) black+=1;
-    if(tree[root]<0 && (tree[root*2+1]<0 || tree[root*2+2]<0)){
+    if(tree[root].v<0 && ((tree[root].left!=-1&&tree[tree[root].left].v<0) || (tree[root].right!=0&&tree[tree[root].right].v<0))){
         is = false;
         return;
     }
-    dfs(root*2+1,black);
-    dfs(root*2+2,black);
+    if(tree[root].left!=-1) dfs(tree[root].left,black);
+    else{
+        if(black!=finalblack) is = false;
+    }
+    if(tree[root].right!=-1) dfs(tree[root].right,black);
+    else{
+        if(black!=finalblack) is = false; 
+    }
 }
 int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         is = true;
-        fill(tree,tree+Size,0);
         fill(pre,pre+Size,0);
+        for(int j=0;j<Size;j++){
+            tree[j].v=tree[j].left=tree[j].right=0;
+        }
         cin>>m;
         for(int j=0;j<m;j++){
             cin>>pre[j];
         }
         if(pre[0]<0) is=false;
-        build(0,m-1,0);
+        build(0,m-1);
         finalblack=0;
-        for(int j=0;tree[j]!=0;j=j*2+1){
-            if(tree[j]>0) finalblack++;
+        for(int j=0;j!=-1;){
+            if(tree[j].v>0) finalblack++;
+            j = tree[j].left;
         }
         dfs(0,0);
         if(is) cout<<"Yes";
